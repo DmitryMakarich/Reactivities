d1e25bfc48a15bfbccfe889fd85a7e988bd765b6
DmitryMakarich <dima.makarich@mail.ru>
2021-08-06 17:12:55 +0300

    style UI

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -18,6 +18,20 @@ export default class ActivityStore {
             Date.parse(a.date) - Date.parse(b.date));
     }
 
+    get groupedActivities() {
+        return Object.entries(
+            this.activitiesByDate.reduce((activities, activity) => {
+                const date = activity.date;
+                activities[date] = activities[date] ? 
+                    [...activities[date], activity] :
+                    [activity];
+                
+                return activities;
+            }, {} as {[key: string]: Activity[]})
+
+        )
+    }
+
     loadActivities = async () => {
         this.loadingInitial = true;
         try {
@@ -43,8 +57,10 @@ export default class ActivityStore {
             try {
                 activity = await agent.Activities.details(id);
                 this.setActivity(activity);
-                this.selectedActivity = activity;
-                this.setLoadingInitial(false);
+                runInAction(() => {
+                    this.selectedActivity = activity;
+                    this.setLoadingInitial(false);
+                })
                 return activity;
             } catch (error) {
                 console.log(error);
