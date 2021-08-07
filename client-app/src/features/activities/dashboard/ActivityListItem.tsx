44777efe578edee84a8af8327bbb816af880b70e
DmitryMakarich <dima.makarich@mail.ru>
2021-08-07 20:53:48 +0300

    handle errors and edit forms

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,3 +1,4 @@
+import { format } from "date-fns";
 import React from "react";
 import { Link } from "react-router-dom";
 import { Button, Icon, Item, Segment } from "semantic-ui-react";
@@ -26,7 +27,7 @@ export default function ActivityListItem({ activity }: Props) {
         </Segment>
         <Segment>
             <span>
-                <Icon name='clock' /> {activity.date}
+                <Icon name='clock' /> {format(activity.date!, 'dd MMM yyyy h:mm aa')}
                 <Icon name='marker'/> {activity.venue}
             </span>
         </Segment>
@@ -38,7 +39,7 @@ export default function ActivityListItem({ activity }: Props) {
                 {activity.description}
             </span>
             <Button 
-                sa={Link}
+                as={Link}
                 to={`/activities/${activity.id}`}
                 color='teal'
                 floated='right'
